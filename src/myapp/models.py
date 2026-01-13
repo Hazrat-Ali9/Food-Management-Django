c2087bb569f927dd2c7ce99234712c99d7600927
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-01-13 12:04:45 +0600

    models  py :rocket:

--- src/myapp/models.py
+++ src/myapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+# models 
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
